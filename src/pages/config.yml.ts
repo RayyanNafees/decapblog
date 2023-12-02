ed1bdcdba381ee45503a5f538719379f45c24a2e
RayyanNafees <nafees.rayyan@gmail.com>
2023-12-02 19:49:18 +0530

    Added Wdgets to homepage

--- src/pages/config.yml.ts
+++ src/pages/config.yml.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from 'astro'
 import { readFile } from 'node:fs/promises'
 
-export const get: APIRoute = async () => {
+export const GET: APIRoute = async () => {
   const yml = await readFile('public/admin/config.yml')
   return new Response(yml)
 }
